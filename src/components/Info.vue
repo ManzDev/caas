704fe6863c5b2bf4e2c53999b1b3870a2ca945cb
ManzDev <JoseRomanHernandez@gmail.com>
2025-05-19 20:59:11 +0100

    Added cat sections to project

---
+++ src/components/Info.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: "Info",
+  data() {
+    return {
+      title: "Manz Component Vue",
+      version: "3.0"
+    }
+  },
+  methods: {
+    welcome() {
+      alert("Hello");
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h2>Títular del componente de Vue</h2>
+    <h3>{{ title }} {{ version }}</h3>
+    <strong>Placeholder</strong>
+  </div>
+</template>
+
+<style>
+.container {
+  background: #318c58;
+  color: #fff;
+  padding: 1rem 3rem;
+}
+</style>
